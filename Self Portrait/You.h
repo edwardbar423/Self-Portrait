3ff444353b32238e63c14344745ca71cc71a7d41
Edward Barber <luckyshot423@gmail.com>
2016-08-17 00:31:23 -0400

    Updated

---
+++ Self Portrait/You.h
@@ -0,0 +1,13 @@
+//
+//  You.h
+//  Self Portrait
+//
+//  Created by Andrew Barber on 8/16/16.
+//  Copyright © 2016 Invictus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface You : NSObject
+
+@end
---
+++ Self Portrait/You.m
@@ -0,0 +1,13 @@
+//
+//  You.m
+//  Self Portrait
+//
+//  Created by Andrew Barber on 8/16/16.
+//  Copyright © 2016 Invictus. All rights reserved.
+//
+
+#import "You.h"
+
+@implementation You
+
+@end
